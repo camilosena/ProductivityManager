c30c1828865322996edfb8b41c2841556ed6e849
Jorge Mario Izquierdo Negrete <jmizquierdo@misena.edu.co>
2015-11-03 21:55:52 -0500

    Clase Session

---
+++ modelo/utilidades/Session.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class Session{
+    
+function Session(){   
+if (empty($_SESSION['rol']) && empty($_SESSION['id'])) {
+    header("location: ../index.php?error=Debe Iniciar Sesión");
+} else {
+    require_once '../modelo/dao/LoginDAO.php';
+    require_once '../facades/FacadeLogin.php';
+    require_once '../modelo/utilidades/Conexion.php';
+    $facadeLogueado = new FacadeLogin;
+    $paginas = $facadeLogueado->seguridadPaginas($_SESSION['rol']);
+    $pagActual = 'agregarAreas.php';
+    $total = count($paginas);
+    foreach ($paginas as $todas) {
+        if ($pagActual != $todas['url']) {
+            $total--;
+        }
+    }
+   if($total==0){
+       header("location: ".$_SESSION['paginaOrigen']. "?errorPermiso=No posee permisos para acceder a este directorio.");       
+   }
+   $_SESSION['paginaOrigen']=$_SERVER['PHP_SELF'];
+} 
+} 
+    
+}

6972d5251d5104ada0d3a464f39fc0e536b2994f
camilosena <Test@MacBook-Pro-de-Camilo.local>
2015-11-29 16:51:17 -0500

    Relación Costos en Información de Proyecto

--- vista/informacionProyecto.php
+++ vista/informacionProyecto.php
@@ -29,17 +29,80 @@ $session->Session($pagActual);
     <?php if (isset($_GET['projectNum'])) { ?>
     <h2 class="h330"><br>Proyecto <?php echo  $_GET['nameProject']; ?>:</h2><br>
         <hr>
-    <?php
-    require_once '../facades/FacadeProductos.php';
+        <div>
+            <?php
+            require_once '../facades/FacadeProductos.php';
     require_once '../modelo/dao/ProductosDAO.php';
     require_once '../modelo/dao/ProyectosDAO.php';
+    require_once '../modelo/dao/estudioCostosDAO.php';
     require_once '../facades/FacadeProyectos.php';
+    require_once '../facades/FacadeEstudioCostos.php';
     require_once '../modelo/utilidades/Conexion.php';
     $facadeProductos = new FacadeProductos;
     $facadeProyecto = new FacadeProyectos;
+    $facadeEstudioCostos = new FacadeEstudioCostos;
+            //  Consultar Proyecto
+            if (isset($_GET['projectNum'])) {
+                $proyectos = $facadeProyecto->consultarProyecto($_GET['projectNum']);
+                if($proyectos['fechaFin']!='0000-00-00'){ $finalDefinida = $proyectos['fechaFin'];}else{$finalDefinida='';}
+                echo '<div id="infoPro">';
+                echo '<table id="muestraDatos"><tr><th colspan="2">Información de Proyecto</th></tr>';
+                echo '<tr><td class="enunciado">Código:</td><td>0' . $proyectos['idProyecto'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Nombre:</td><td>' . $proyectos['nombreProyecto'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Fecha Inicio:</td><td>' . $proyectos['fechaInicio'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Fecha Fin:</td><td> ' . $finalDefinida . '</td></tr>';
+                echo '<tr><td class="enunciado">Estado:</td><td>' . $proyectos['estadoProyecto'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Ejecutado:</td><td>' . $proyectos['ejecutado'] . '%</td></tr>';
+                echo '<tr><td class="enunciado">Observaciones:</td><td>' . $proyectos['observaciones'] . '</td></tr>';
+                $comi = "'";
+                if ($proyectos['estadoProyecto'] != 'Ejecucion') {
+                    echo '<tr><td class="enunciado">Opciones:</td><td>';
+                    echo '<a class="me" title="Modificar Proyecto"href="javascript:modificarProyecto(' . $comi . 'modificarProyecto.php?idProject='. $proyectos['idProyecto'] . $comi . ');"><img class="iconos" src="../img/modify.png"></a>';
+                }
+                if ($proyectos['estadoProyecto'] == 'Sin Estudio Costos') {
+                    echo '<a class="me" title="Generar Estudio de Costos" href="javascript:estudioCostos(' . $comi . 'estudioDeCostos.php?projectNum=' . $proyectos['idProyecto'] . '&nameProject=' . $proyectos['nombreProyecto'] . $comi . ');"><img class="iconos" src="../img/costos.png"></a>';
+                } elseif ($proyectos['estadoProyecto'] == 'Sin Produccion') {
+                    echo '<a class="me" title="Incluir Producción" href="javascript:produccionProyecto(' . $comi . 'produccionProyecto.php?projectNum=' . $proyectos['idProyecto'] . '&nameProject=' . $proyectos['nombreProyecto'] . $comi . ');"><img class="iconos" src="../img/products.png"></a>';
+
+                }
+                require_once '../modelo/utilidades/Conexion.php';
+                $facadeProyecto = new FacadeProyectos;
+                $clie = $facadeProyecto->clienteAsignado($proyectos['idProyecto']);
+                echo '</table>';
+                echo '</div><div id="infoClient">';
+                echo '<table id="muestraDatos"><tr><th colspan="2">Datos de Cliente</th></tr>';
+                echo '<tr><td class="enunciado">Código:</td><td>0' . $clie['idCliente'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Empresa:</td><td>' . $clie['nombreCompania'] . '</td></tr>';
+                echo '<tr><td class="enunciado">NIT:</td><td>' . $clie['nit'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Sector Empresarial:</td><td>' . $clie['sectorEmpresarial'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Sector Económico:</td><td>' . $clie['sectorEconomico'] . '</td></tr>';
+                echo '<tr><td class="enunciado">PBX:</td><td>' . $clie['telefonoFijo'] . '</td></tr>';
+                echo '<tr><th colspan="2">Representante Legal</th></tr>';
+                echo '<tr><td class="enunciado">Identificación:</td><td> ' . $clie['identificacion'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Nombre:</td><td>' . $clie['nombre'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Dirección:</td><td>' . $clie['direccion'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Teléfono:</td><td>' . $clie['telefono'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Correo Electronico:</td><td>' . $clie['email'] . '</td></tr>';
+                echo '</table>';
+                echo '</div><div id="infoGere">';
+                $pro = $facadeProyecto->gerenteDeProyecto($proyectos['idProyecto']);
+                echo '<table id="muestraDatos"><tr><th colspan="2">Gerente Encargado</th></tr>';
+                echo '<tr><td class="enunciado">Código Gerente:</td><td>0' . $pro['idUsuario'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Nombre:</td><td>' . $pro['nombre'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Fecha Inicio:</td><td>' . $pro['direccion'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Fecha Fin:</td><td> ' . $pro['telefono'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Estado:</td><td>' . $pro['email'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Ejecutado:</td><td>' . $pro['nombreArea'] . '</td></tr>';
+                echo '</table>';
+                echo '</div>';
+            }
+            ?>
+        </div>
+    <?php
     $products = $facadeProyecto->obtenerDatoProductoProyecto($_GET['projectNum']);
     if(count($products)>=1){?>
         <div>
+        <br><br>
             <strong><h2 class="h331" style="margin-left:14%;">Productos Requeridos:</h2></strong><br>
             <table class="tableSection">
                 <thead>
@@ -74,67 +137,25 @@ $session->Session($pagActual);
                 </tbody>
             </table>
         </div>
-    <?php }?>
         <div>
             <?php
-
-            //  Consultar Proyecto
-            if (isset($_GET['projectNum'])) {
-                $proyectos = $facadeProyecto->consultarProyecto($_GET['projectNum']);
-                if($proyectos['fechaFin']!='0000-00-00'){ $finalDefinida = $proyectos['fechaFin'];}else{$finalDefinida='';}
-                echo '<div id="infoPro">';
-                echo '<table id="muestraDatos"><tr><th colspan="2">Información de Proyecto</th></tr>';
-                echo '<tr><td class="enunciado">Código:</td><td>0' . $proyectos['idProyecto'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Nombre:</td><td>' . $proyectos['nombreProyecto'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Fecha Inicio:</td><td>' . $proyectos['fechaInicio'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Fecha Fin:</td><td> ' . $finalDefinida . '</td></tr>';
-                echo '<tr><td class="enunciado">Estado:</td><td>' . $proyectos['estadoProyecto'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Ejecutado:</td><td>' . $proyectos['ejecutado'] . '%</td></tr>';
-                echo '<tr><td class="enunciado">Observaciones:</td><td>' . $proyectos['observaciones'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Opciones:</td><td>';
-                $comi = "'";
-                if ($proyectos['estadoProyecto'] != 'Ejecucion') {
-                    echo '<a class="me" title="Modificar Proyecto"href="javascript:modificarProyecto(' . $comi . 'modificarProyecto.php?idProject='. $proyectos['idProyecto'] . $comi . ');"><img class="iconos" src="../img/modify.png"></a>';
-                }
-                if ($proyectos['estadoProyecto'] == 'Sin Estudio Costos') {
-                    echo '<a class="me" title="Generar Estudio de Costos" href="javascript:estudioCostos(' . $comi . 'estudioDeCostos.php?projectNum=' . $proyectos['idProyecto'] . '&nameProject=' . $proyectos['nombreProyecto'] . $comi . ');"><img class="iconos" src="../img/costos.png"></a>';
-                } elseif ($proyectos['estadoProyecto'] == 'Sin Produccion') {
-                    echo '<a class="me" title="Incluir Producción" href="javascript:produccionProyecto(' . $comi . 'produccionProyecto.php?projectNum=' . $proyectos['idProyecto'] . '&nameProject=' . $proyectos['nombreProyecto'] . $comi . ');"><img class="iconos" src="../img/products.png"></a>';
-
-                }
-                require_once '../modelo/utilidades/Conexion.php';
-                $facadeProyecto = new FacadeProyectos;
-                $clie = $facadeProyecto->clienteAsignado($proyectos['idProyecto']);
-                echo '</table>';
-                echo '</div><div id="infoClient">';
-                echo '<table id="muestraDatos"><tr><th colspan="2">Datos de Cliente</th></tr>';
-                echo '<tr><td class="enunciado">Código:</td><td>0' . $clie['idCliente'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Empresa:</td><td>' . $clie['nombreCompania'] . '</td></tr>';
-                echo '<tr><td class="enunciado">NIT:</td><td>' . $clie['nit'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Sector Empresarial:</td><td>' . $clie['sectorEmpresarial'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Sector Económico:</td><td>' . $clie['sectorEconomico'] . '</td></tr>';
-                echo '<tr><td class="enunciado">PBX:</td><td>' . $clie['telefonoFijo'] . '</td></tr>';
-                echo '<tr><td colspan="2" style="text-align:center">Representante Legal</td></tr>';
-                echo '<tr><td class="enunciado">Identificación:</td><td> ' . $clie['identificacion'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Nombre:</td><td>' . $clie['nombre'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Dirección:</td><td>' . $clie['direccion'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Teléfono:</td><td>' . $clie['telefono'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Correo Electronico:</td><td>' . $clie['email'] . '</td></tr>';
-                echo '</table>';
-                echo '</div><div id="infoGere">';
-                $pro = $facadeProyecto->gerenteDeProyecto($proyectos['idProyecto']);
-                echo '<table id="muestraDatos"><tr><th colspan="2">Gerente Encargado</th></tr>';
-                echo '<tr><td class="enunciado">Código Gerente:</td><td>0' . $pro['idUsuario'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Nombre:</td><td>' . $pro['nombre'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Fecha Inicio:</td><td>' . $pro['direccion'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Fecha Fin:</td><td> ' . $pro['telefono'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Estado:</td><td>' . $pro['email'] . '</td></tr>';
-                echo '<tr><td class="enunciado">Ejecutado:</td><td>' . $pro['nombreArea'] . '</td></tr>';
+                echo '<div id="infoGere">';
+                $costosProyecto = $facadeEstudioCostos->verificaExistenciaEstudio($proyectos['idProyecto']);
+                echo '<table id="muestraDatos"><tr><th colspan="2">Relación de Costos</th></tr>';
+                echo '<tr><td class="enunciado">Mano de Obra:</td><td>$ ' . $costosProyecto['costoManoDeObra'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Materia Prima / Insumos:</td><td>$ ' . $costosProyecto['costoProduccion'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Utilidad:</td><td>$ ' . $costosProyecto['utilidad'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Tiempo Estimado (Horas):</td><td> ' . $costosProyecto['tiempoEstimado'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Empleados Solicitados:</td><td> ' . $costosProyecto['totalTrabajadores'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Total Proyecto:</td><td>$ ' . $costosProyecto['costoProyecto'] . '</td></tr>';
+                echo '<tr><td class="enunciado">Observaciones:</td><td>' . $costosProyecto['observaciones'] . '</td></tr>';
                 echo '</table>';
                 echo '</div>';
-            }
             ?>
         </div>
+
+    <?php }?>
+        
         <hr>
     <script language=javascript>
         function modificarProyecto(URL) {
